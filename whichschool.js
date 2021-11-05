fb01f654c2a32fbd91e15b1a0e951dbbf9ad764a
Shivani Sharma <shivani1406sharma@gmail.com>
2021-11-05 20:58:35 +0000

    new js

---
+++ whichschool.js
@@ -0,0 +1,21 @@
+const whichSchool  = function (age) {
+  if(age<13)
+  {
+    return "Elementary School";
+  }
+  else if(age>=13 && age<=18)
+  {
+    return "Secondary School";
+  }
+  else
+  {
+    return "Lighthouse Labs";
+  }
+  // Your code in here ...
+}
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
